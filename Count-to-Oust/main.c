94e25bbe509fadd87b288f8175c6636a488c49aa
dirk <dirkadin@gmail.com>
2018-10-11 15:57:13 -0500

    Initial Commit

---
+++ Count-to-Oust/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Count-to-Oust
+//
+//  Created by Dirk on 10/11/18.
+//  Copyright © 2018 Dirk. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
