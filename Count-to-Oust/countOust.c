cf442bc183ec22d941f31247b6f0e9cb5d311617
dirk <dirkadin@gmail.com>
2018-10-11 15:59:26 -0500

    Added required files

---
+++ Count-to-Oust/countOust.c
@@ -0,0 +1,9 @@
+//
+//  countOust.c
+//  Count-to-Oust
+//
+//  Created by Dirk on 10/11/18.
+//  Copyright © 2018 Dirk. All rights reserved.
+//
+
+#include "countOust.h"
---
+++ Count-to-Oust/countOust.h
@@ -0,0 +1,14 @@
+//
+//  countOust.h
+//  Count-to-Oust
+//
+//  Created by Dirk on 10/11/18.
+//  Copyright © 2018 Dirk. All rights reserved.
+//
+
+#ifndef countOust_h
+#define countOust_h
+
+#include <stdio.h>
+
+#endif /* countOust_h */
